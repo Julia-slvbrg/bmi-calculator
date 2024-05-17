df42d3165260e7a15c59f6450806f36e2d1dff34
Julia-slvbrg <julia.slvb@gmail.com>
2024-05-17 18:46:09 -0300

    chore: basic structure defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 
-import './App.css'
+import { useState } from 'react';
+import Form from './components/Form';
+import './App.css';
 
 function App() {
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [bmi, setBmi] = useState();
+
+  
 
   return (
     <>
-      
+      <h1>
+        Calcule seu IMC
+      </h1>
+      <form>
+        <div>
+          <label>Altura (m):</label>
+          <input type='number' required />
+        </div>
+        
+        <div>
+          <label>Peso (kg):</label>
+          <input type='number' required />
+        </div>
+        <button type="submit">Medir IMC</button>
+      </form>
     </>
   )
 }
